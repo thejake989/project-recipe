6fcaada36cbcfff45cca3b3d276af8ffa7749e58
MattGrizzly <magrishams@gmail.com>
2022-05-04 18:12:43 -0500

    dropdown fix

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
-  });
+var dropdowns = document.querySelectorAll('.dropdown-trigger')
+for (var i = 0; i < dropdowns.length; i++){
+    M.Dropdown.init(dropdowns[i]);
+}
